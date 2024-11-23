6d36996e889c015de2e4b62c6663895570affe6e
Daniel Grondin <king@numsgil.co>
2024-11-23 09:51:26 -0500

    Reduced `NavbarLinkProps` type to only what's needed

--- src/components/NavbarLink.tsx
+++ src/components/NavbarLink.tsx
@@ -1,10 +1,10 @@
-import { NavLink, type LinkProps } from "react-router-dom";
+import { type NavigateOptions, NavLink, type To } from "react-router-dom";
 import { Button } from "@mantine/core";
 
 // @ts-types="@types/react"
 import type { FC, PropsWithChildren } from "react";
 
-export type NavbarLinkProps = PropsWithChildren<LinkProps>;
+export type NavbarLinkProps = PropsWithChildren<{ to: To } & NavigateOptions>;
 
 export const NavbarLink: FC<NavbarLinkProps> = ({ children, ...props }) => {
     return (
