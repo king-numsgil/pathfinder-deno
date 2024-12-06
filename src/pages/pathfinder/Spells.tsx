b90f4fcdf4e0b139052ae8debda522bfecbc77e0
Daniel Grondin <king@numsgil.co>
2024-12-05 21:48:53 -0500

    Now listing spells! The start of it anyway...

--- src/pages/pathfinder/Spells.tsx
+++ src/pages/pathfinder/Spells.tsx
@@ -1,8 +1,37 @@
+import useSwr from "swr";
+
 // @ts-types="@types/react"
-import { type FC } from "react";
+import { type FC, Suspense } from "react";
+
+import { spellFetcher } from "@/client.tsx";
+
+const SpellPage: FC = () => {
+    const {data} = useSwr(
+        "spell-list",
+        spellFetcher({
+            query: {},
+        }),
+        {
+            suspense: true,
+        },
+    );
+
+    return (
+        <>
+            {data.items.map((spell, index) => (
+                <h1 key={index}>{spell.name}</h1>
+            ))}
+        </>
+    );
+}
 
 export const Spells: FC = () => {
-    return <>
-        <h1>Vite + React</h1>
-    </>;
+    return (
+        <>
+            <h1>Vite + React</h1>
+            <Suspense fallback={<h2>Loading...</h2>}>
+                <SpellPage />
+            </Suspense>
+        </>
+    );
 };
