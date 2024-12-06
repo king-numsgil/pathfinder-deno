b8da52adddf8118ac6a1e677bc25e7ef0442d986
Daniel Grondin <king@numsgil.co>
2024-12-05 20:56:19 -0500

    Enabled sourcemaps, maybe it'll help debugging a bit

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,9 @@ export default defineConfig({
         react(),
         vanillaExtractPlugin(),
     ],
+    build: {
+        sourcemap: "inline",
+    },
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "./src"),
