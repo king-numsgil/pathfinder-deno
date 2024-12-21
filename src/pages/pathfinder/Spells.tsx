81d37ccc94cccaf6778eed7e7817488f08ac52bc
Daniel Grondin <king@numsgil.co>
2024-12-21 11:55:00 -0500

    Moved Spells to `useSuspenseInfiniteQuery`

--- src/pages/pathfinder/Spells.tsx
+++ src/pages/pathfinder/Spells.tsx
@@ -1,30 +1,52 @@
-import { useSuspenseQuery } from "@tanstack/react-query";
+import { type DefaultError, type InfiniteData, type QueryKey, useSuspenseInfiniteQuery } from "@tanstack/react-query";
+import { Button, Flex, Loader, Title } from "@mantine/core";
 
 // @ts-types="@types/react"
 import { type FC, Suspense } from "react";
 
-import { spellFetcher, type SpellRequest } from "@/client.tsx";
+import { spellFetcher, type SpellRequest, type SpellResult } from "@/client.tsx";
 
 type SpellPageProps = {
     filters: Omit<SpellRequest["query"], "cursor" | "pageLength">;
 };
 
 const SpellPage: FC<SpellPageProps> = ({ filters }) => {
-    const { data, isFetching } = useSuspenseQuery({
+    const { data, isFetching, hasNextPage, fetchNextPage } = useSuspenseInfiniteQuery<
+        SpellResult,
+        DefaultError,
+        InfiniteData<SpellResult>,
+        QueryKey,
+        string | undefined
+    >({
         queryKey: ["spell-list", filters],
-        queryFn: () => {
-            return spellFetcher(filters);
+        initialPageParam: undefined,
+        queryFn: (ctx) => {
+            return spellFetcher({
+                cursor: ctx.pageParam,
+                ...filters,
+            });
         },
+        getNextPageParam: (lastPage) => lastPage.endCursor ?? undefined,
     });
 
     return (
         <>
-            {isFetching ? <h1>Loading...</h1> : null}
+            <Title order={1}>
+                Pathfinder Spells
+                {isFetching ? <Loader size="sm" style={{ marginLeft: "10px" }} /> : null}
+            </Title>
             {data
                 ? (
-                    <ul>
-                        {data.items.map((spell, index) => <ul key={index}>{spell.name}</ul>)}
-                    </ul>
+                    <>
+                        <ul>
+                            {data.pages.map((page, pageIndex) => (
+                                page.items.map((spell, index) => <ul key={pageIndex * 10 + index}>{spell.name}</ul>)
+                            ))}
+                        </ul>
+                        <Button onClick={() => fetchNextPage()} disabled={!hasNextPage}>
+                            Load More
+                        </Button>
+                    </>
                 )
                 : null}
         </>
@@ -33,11 +55,14 @@ const SpellPage: FC<SpellPageProps> = ({ filters }) => {
 
 export const Spells: FC = () => {
     return (
-        <>
-            <h1>Vite + React</h1>
-            <Suspense fallback={<h2>Loading...</h2>}>
-                <SpellPage filters={{}} />
-            </Suspense>
-        </>
+        <Suspense
+            fallback={
+                <Flex gap="md" justify="center" align="center">
+                    <Loader size="xl" />
+                </Flex>
+            }
+        >
+            <SpellPage filters={{}} />
+        </Suspense>
     );
 };
